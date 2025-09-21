a4e0ad61e5614a0814c5e8370f56446ccc3cce30
Aryan Choudhary <aryanchoudhary3907@gmail.com>
2025-09-21 16:33:41 +0530

    Partial Implementation of Service

--- src/main/java/com/myproject/StayBuddy/services/RoomService.java
+++ src/main/java/com/myproject/StayBuddy/services/RoomService.java
@@ -1,7 +1,10 @@
 package com.myproject.StayBuddy.services;
 
+import com.myproject.StayBuddy.repositories.BookingRepository;
 import com.myproject.StayBuddy.repositories.RoomRepository;
+import com.myproject.StayBuddy.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
