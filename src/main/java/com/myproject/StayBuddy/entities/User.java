6fcefb38f0bf987f8d8b79900ffee5abb98061a3
Aaditya <developer.aadi@outlook.com>
2025-09-21 13:15:39 +0530

    Id change to userId

--- src/main/java/com/myproject/StayBuddy/entities/User.java
+++ src/main/java/com/myproject/StayBuddy/entities/User.java
@@ -15,7 +15,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long userId;
 
     private String name;
     private LocalDate DOB;
