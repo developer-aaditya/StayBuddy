5f03a2e76cff6b4a813295a4c4bbd45bfaff6de0
Aaditya <developer.aadi@outlook.com>
2025-09-21 11:52:57 +0530

    Initail Commit

---
+++ src/main/java/com/myproject/StayBuddy/StayBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.StayBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayBuddyApplication.class, args);
+	}
+
+}
